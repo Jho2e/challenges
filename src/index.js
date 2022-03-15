cf64f376b6c9558b65c99035663a21616e9a08ca
Jho2e <ohjaehokor@naver.com>
2022-03-15 16:01:35 +0900

    바닐라 JS 챌린지
    숫자게임
    (4.0~4.7)

--- src/index.js
+++ src/index.js
@@ -1,55 +1,21 @@
-const yyyy = document.querySelector("body");
-const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
+const numberInputBetween = document.querySelector("#numberSubmitBtn");
+const result = document.querySelector("#result");
+const usergenerateNumber = document.querySelector(".generateNumberBetween");
+const userGuessNumber = document.querySelector("#guessTheNumber");
 
-function handleResize() {
-  var width = window.innerWidth;
+const HIDDEN_CLASSNAME = "hidden";
 
-  if (width <= 200) {
-    yyyy.style.backgroundColor = colors[1];
-  } else if (width > 200 && width <= 400) {
-    yyyy.style.backgroundColor = colors[2];
-  } else if (width > 400 && width <= 600) {
-    yyyy.style.backgroundColor = colors[3];
-  } else if (width > 600 && width <= 800) {
-    yyyy.style.backgroundColor = colors[4];
+function numberSubmit(event) {
+  event.preventDefault();
+  const guessNumber = userGuessNumber.value;
+  const computerNumber = Math.ceil(Math.random() * usergenerateNumber.value);
+
+  if (parseInt(computerNumber, 10) === parseInt(guessNumber, 10)) {
+    result.innerText = `You chose: ${guessNumber}, the machine chose: ${computerNumber}.\nYou won!`;
+  } else {
+    result.innerText = `You chose: ${guessNumber}, the machine chose: ${computerNumber}.\nYou lost!`;
   }
+  result.classList.remove(HIDDEN_CLASSNAME);
 }
 
-window.addEventListener("resize", handleResize);
-
-/* 
-const title = document.querySelector("body");
-const colors = [
-  "#1abc9c",
-  "#3498db",
-  "#9b59b6",
-  "#f39c12",
-  "#e74c3c",
-  "#C18282",
-  "#FF50CF",
-  "#0A9696",
-];
-const RESIZE_CLASS = (title.style.backgroundColor = colors[0]);
-
-function handleResize() {
-  var wframe = window.innerWidth;
-  console.log(wframe);
-  if (wframe <= 200) {
-    title.style.backgroundColor = colors[1];
-  } else if (wframe > 200 && wframe <= 400) {
-    title.style.backgroundColor = colors[2];
-  } else if (wframe > 400 && wframe <= 600) {
-    title.style.backgroundColor = colors[3];
-  } else if (wframe > 600 && wframe <= 800) {
-    title.style.backgroundColor = colors[4];
-  } else if (wframe > 800 && wframe <= 1000) {
-    title.style.backgroundColor = colors[6];
-  } else if (wframe > 1000 && wframe <= 1200) {
-    title.style.backgroundColor = colors[6];
-  } else if (wframe > 1200 && wframe <= 1400) {
-    title.style.backgroundColor = colors[7];
-  }
-}
-
-window.addEventListener("resize", handleResize);
- */
+numberInputBetween.addEventListener("submit", numberSubmit);
