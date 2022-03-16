7c1c68731c447c52a155f4c778f5154d2cebb327
Jho2e <ohjaehokor@naver.com>
2022-03-16 11:18:22 +0900

    date와 math floor 사용
    22/3/16
    date와 math floor를 사용해서
    특정 날짜까지 남은 시간을
    실시간으로 카운트다운

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-const numberInputBetween = document.querySelector("#numberSubmitBtn");
-const result = document.querySelector("#result");
-const usergenerateNumber = document.querySelector(".generateNumberBetween");
-const userGuessNumber = document.querySelector("#guessTheNumber");
+const clockTitle = document.querySelector(".js-clock");
 
-const HIDDEN_CLASSNAME = "hidden";
+function getClock() {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-function numberSubmit(event) {
-  event.preventDefault();
-  const guessNumber = userGuessNumber.value;
-  const computerNumber = Math.ceil(Math.random() * usergenerateNumber.value);
+  const christmasEve = new Date(2022, 11, 25, 0, 0, 0);
+  const now = new Date();
+  const remain = christmasEve - now;
 
-  if (parseInt(computerNumber, 10) === parseInt(guessNumber, 10)) {
-    result.innerText = `You chose: ${guessNumber}, the machine chose: ${computerNumber}.\nYou won!`;
-  } else {
-    result.innerText = `You chose: ${guessNumber}, the machine chose: ${computerNumber}.\nYou lost!`;
-  }
-  result.classList.remove(HIDDEN_CLASSNAME);
+  /* Date - Date 를 했을 때 나오는 숫자의 의미는
+    " XXXXXXXXXXX초.XXX밀리초 "이다
+    이것을 아래처럼 하면 일,시간,분,초 로 변환할 수 있다.
+  */
+
+  const days = Math.floor(remain / day);
+  const hours = Math.floor((remain % day) / hour);
+  const minutes = Math.floor((remain % hour) / minute);
+  const seconds = Math.floor((remain % minute) / second);
+
+  clockTitle.innerText = `${days}d:${hours}h:${minutes}m:${seconds}s`;
 }
 
-numberInputBetween.addEventListener("submit", numberSubmit);
+getClock();
+setInterval(getClock, 1000);
